9aa652a731ce205cdc8cf6e0b27aa474324954ac
Matt Price <pricemattm@gmail.com>
2013-07-08 20:10:42 -0500

    refactor tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -11,8 +11,7 @@ describe "Static pages" do
 
     before { visit root_path } 
 
-    it { should have_selector('h1', text: 'Sample App') }
-    it { should have_selector('title', text: full_title('')) }
+    test_page_title('', 'Sample App')
     it { should_not have_selector 'title', text: '| Home' }
   
   end
@@ -32,7 +31,21 @@ describe "Static pages" do
     test_page_title('Contact Us')
   end
 
-  
-  
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    page.should have_selector 'title', text: full_title('About Us')
+    click_link "Help"
+    page.should have_selector('title', text: full_title('Help'))
+    click_link "Contact"
+    page.should have_selector('title', text: full_title('Contact Us'))
+    click_link "Home"
+    page.should have_selector('title', text: full_title(''))
+    click_link "Sign up now!"
+    page.should have_selector('title', text: full_title('Sign up'))
+    click_link "sample app"
+    page.should have_selector('title', text: full_title(''))
+    
+  end
 
 end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,15 +1,20 @@
-def full_title(page_title)
+include ApplicationHelper
 
-	base_title = "Ruby on Rails Tutorial Sample App"
-	if page_title.empty? 
-		base_title 
-	else
-		"#{base_title} | #{page_title}"
+# def full_title(page_title)
+
+# 	base_title = "Ruby on Rails Tutorial Sample App"
+# 	if page_title.empty? 
+# 		base_title 
+# 	else
+# 		"#{base_title} | #{page_title}"
+# 	end
+# end
+
+def test_page_title(*args)
+	header = args[0]
+	if args.size == 2
+		header = args[1]
 	end
-end
-
-
-def test_page_title(page_title)
-	it { should have_selector('h1', text: page_title) }
-    it { should have_selector('title', text: full_title(page_title)) }
+ 	it { should have_selector('h1', text: header) }
+    it { should have_selector('title', text: full_title(args[0])) }
 end
