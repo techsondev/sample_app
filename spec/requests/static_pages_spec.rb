04e67b0d6c6d47578bcacf1b53095b5dd0061322
Matt Price <pricemattm@gmail.com>
2013-07-13 19:28:32 -0500

    Finish sign in

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -45,7 +45,6 @@ describe "Static pages" do
     page.should have_selector('title', text: full_title('Sign up'))
     click_link "sample app"
     page.should have_selector('title', text: full_title(''))
-    
   end
 
 end
